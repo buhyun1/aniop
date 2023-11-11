042d6a3216e742200fdd3a757df27ad349043d28
JeongSuHwan <akdtlfdl1014@naver.com>
2023-11-11 20:43:08 +0900

    feat : 화면 기본 구성 사이드 바 추가

---
+++ frontend/src/components/sidebar_widget.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "sidebar">
+    </div>
+</template>
+
+<style>
+    .sidebar{
+        border-top-right-radius: 30px;
+        background-color: #D9D9D9;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        background: rgba(217, 217, 217, 0.5);;
+    }
+
+    @media (min-width: 768px) {
+        .sidebar {
+            width: 340px;
+            height: 80vh;
+        }
+    }
+
+    @media (max-width: 767px) {
+        .sidebar {
+            width: 240px; 
+            height: 60vh; 
+        }
+    }
+</style>
