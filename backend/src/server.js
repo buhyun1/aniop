64da569df6aec4045a05b32a3a0b07a54b1c983a
buhyun <102472949+buhyun1@users.noreply.github.com>
2023-11-14 16:22:34 +0900

    feat : backend 연결 및 디비 쿼리 js 파일 업로드

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,25 @@
+// 메인 서버 파일 (예: index.js)
 require('dotenv').config();
-
 const { Client } = require('ssh2');
-const mysql = require('mysql2');
-
+const mysql = require('mysql2/promise');
+const express = require('express');
+const { getArticles } = require('./query'); // query.js 모듈 가져오기
+const app = express();
 const sshClient = new Client();
+
+let db;
 
 sshClient.on('ready', () => {
   sshClient.forwardOut(
     '127.0.0.1', // 소스 주소
-    22, // 임의의 소스 포트
+    12345, // 임의의 소스 포트
     process.env.RDS_HOST, // RDS 인스턴스의 내부 IP
     3306, // RDS MySQL 서버의 포트
-    (err, stream) => {
+    async (err, stream) => {
       if (err) throw err;
 
       // MySQL 연결 설정
-      const db = mysql.createConnection({
+      db = await mysql.createConnection({
         host: '127.0.0.1', // 로컬 호스트
         port: 3306,
         user: process.env.RDS_USER,
@@ -24,37 +28,8 @@ sshClient.on('ready', () => {
         stream: stream
       });
 
-      // MySQL에 연결
-      db.connect(err => {
-        if (err) {
-          console.error('Database connection failed: ' + err.stack);
-          return;
-        }
-        console.log('Connected to database');
-
-        // articles 테이블에서 데이터를 가져오는 SQL 쿼리 실행
-        db.query('SELECT * FROM Articles', (err, results) => {
-          if (err) {
-            console.error('Error executing query: ' + err.stack);
-            return;
-          }
-
-          // 결과 출력
-          console.log('Fetched ' + results.length + ' rows');
-          results.forEach(row => {
-            console.log(row); // 각 행의 데이터 출력
-          });
-
-          // 연결 종료
-          db.end(err => {
-            if (err) {
-              console.error('Error closing connection: ' + err.stack);
-              return;
-            }
-            console.log('Connection closed');
-          });
-        });
-      });
+      // 연결 성공 로그
+      console.log('Connected to database'); // 데이터베이스 연결 성공 메시지
     }
   );
 }).connect({
@@ -62,4 +37,45 @@ sshClient.on('ready', () => {
   port: 22,
   username: process.env.SSH_USER,
   privateKey: require('fs').readFileSync(process.env.SSH_PRIVATE_KEY_PATH)
+});
+
+// ... 나머지 Express 서버 및 라우팅 코드 ...
+app.get('/articles', async (req, res) => {
+  try {
+    const articles = await getArticles(db);
+    console.log('Fetched Articles:', articles); // 쿼리 결과 출력
+    res.json(articles);
+  } catch (err) {
+    console.error('Error executing query: ' + err.stack); // 에러 로그
+    res.status(500).send('Error fetching articles');
+  }
+});
+
+/*
+//POST 라우트를 설정하여 날짜에 해당하는 데이터를 검색
+const { getArticlesByDate } = require('./query');
+
+app.use(express.json()); // JSON 본문 파싱을 위한 미들웨어
+
+app.post('/articlesByDate', async (req, res) => {
+  const date = req.body.date;
+  
+  if (!date) {
+    return res.status(400).send('No date provided');
+  }
+
+  try {
+    const articles = await getArticlesByDate(db, date);
+    res.json(articles);
+  } catch (err) {
+    console.error('Error executing query: ' + err.stack);
+    res.status(500).send('Error fetching articles');
+  }
+});
+ */
+
+// 서버 시작
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
