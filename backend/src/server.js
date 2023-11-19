fb7a000fc01833f8073b0b202207982db2bc0c61
jinha <75898490+iububble@users.noreply.github.com>
2023-11-19 21:52:49 +0900

    chore : 주석 해제

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const { getArticles } = require('./query'); // query.js 모듈 가져오기
 const app = express();
 const path = require('path');
 
-// const sshClient = new Client();
+const sshClient = new Client();
 
 let db;
 
@@ -32,35 +32,35 @@ app.get('*', (req, res) => {
   }
 });
 
-// sshClient.on('ready', () => {
-//   sshClient.forwardOut(
-//     '127.0.0.1', // 소스 주소
-//     12345, // 임의의 소스 포트
-//     process.env.RDS_HOST, // RDS 인스턴스의 내부 IP
-//     3306, // RDS MySQL 서버의 포트
-//     async (err, stream) => {
-//       if (err) throw err;
+sshClient.on('ready', () => {
+  sshClient.forwardOut(
+    '127.0.0.1', // 소스 주소
+    12345, // 임의의 소스 포트
+    process.env.RDS_HOST, // RDS 인스턴스의 내부 IP
+    3306, // RDS MySQL 서버의 포트
+    async (err, stream) => {
+      if (err) throw err;
 
-//       // MySQL 연결 설정
-//       db = await mysql.createConnection({
-//         host: '127.0.0.1', // 로컬 호스트
-//         port: 3306,
-//         user: process.env.RDS_USER,
-//         password: process.env.RDS_PASSWORD,
-//         database: process.env.RDS_DATABASE,
-//         stream: stream
-//       });
+      // MySQL 연결 설정
+      db = await mysql.createConnection({
+        host: '127.0.0.1', // 로컬 호스트
+        port: 3306,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE,
+        stream: stream
+      });
 
-//       // 연결 성공 로그
-//       console.log('Connected to database'); // 데이터베이스 연결 성공 메시지
-//     }
-//   );
-// }).connect({
-//   host: process.env.SSH_HOST, // SSH 서버의 퍼블릭 IP
-//   port: 22,
-//   username: process.env.SSH_USER,
-//   privateKey: require('fs').readFileSync(process.env.SSH_PRIVATE_KEY_PATH)
-// });
+      // 연결 성공 로그
+      console.log('Connected to database'); // 데이터베이스 연결 성공 메시지
+    }
+  );
+}).connect({
+  host: process.env.SSH_HOST, // SSH 서버의 퍼블릭 IP
+  port: 22,
+  username: process.env.SSH_USER,
+  privateKey: require('fs').readFileSync(process.env.SSH_PRIVATE_KEY_PATH)
+});
 
 // ... 나머지 Express 서버 및 라우팅 코드 ...
 app.get('/articles', async (req, res) => {
