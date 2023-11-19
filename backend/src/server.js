8aee24d268b84c8289fb59c91796985db109c750
buhyun <102472949+buhyun1@users.noreply.github.com>
2023-11-19 21:46:37 +0900

    feat : git pull 업데이트

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,15 @@ const { getArticles, getArticlesByDate, getArticleById } = require('./query'); /
 const app = express();
 const path = require('path');
 
-// const sshClient = new Client();
+const sshClient = new Client();
 
+console.log(process.env.SSH_PRIVATE_KEY_PATH);
 let db;
 
 // API 라우트 예시
 app.use('/api', (req, res) => {
   res.json({ message: '이것은 API 응답입니다.' });
 });
-<<<<<<< HEAD
-=======
 // Vue.js 빌드 파일을 정적 파일로 제공
 // Dockerfile에서 복사된 위치를 기반으로 경로 설정
 app.use(express.static('dist'));
@@ -32,37 +31,35 @@ app.get('*', (req, res) => {
     console.log(`404 - API not found for path: ${req.path}`);
   }
 });
->>>>>>> 1ae337f027553786a20b7f8f126b18a3bed88231
+sshClient.on('ready', () => {
+  sshClient.forwardOut(
+    '127.0.0.1', // 소스 주소
+    12345, // 임의의 소스 포트
+    process.env.RDS_HOST, // RDS 인스턴스의 내부 IP
+    3306, // RDS MySQL 서버의 포트
+    async (err, stream) => {
+      if (err) throw err;
 
-// sshClient.on('ready', () => {
-//   sshClient.forwardOut(
-//     '127.0.0.1', // 소스 주소
-//     12345, // 임의의 소스 포트
-//     process.env.RDS_HOST, // RDS 인스턴스의 내부 IP
-//     3306, // RDS MySQL 서버의 포트
-//     async (err, stream) => {
-//       if (err) throw err;
+      // MySQL 연결 설정
+      db = await mysql.createConnection({
+        host: '127.0.0.1', // 로컬 호스트
+        port: 3306,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE,
+        stream: stream
+      });
 
-//       // MySQL 연결 설정
-//       db = await mysql.createConnection({
-//         host: '127.0.0.1', // 로컬 호스트
-//         port: 3306,
-//         user: process.env.RDS_USER,
-//         password: process.env.RDS_PASSWORD,
-//         database: process.env.RDS_DATABASE,
-//         stream: stream
-//       });
-
-//       // 연결 성공 로그
-//       console.log('Connected to database'); // 데이터베이스 연결 성공 메시지
-//     }
-//   );
-// }).connect({
-//   host: process.env.SSH_HOST, // SSH 서버의 퍼블릭 IP
-//   port: 22,
-//   username: process.env.SSH_USER,
-//   privateKey: require('fs').readFileSync(process.env.SSH_PRIVATE_KEY_PATH)
-// });
+      // 연결 성공 로그
+      console.log('Connected to database'); // 데이터베이스 연결 성공 메시지
+    }
+  );
+}).connect({
+  host: process.env.SSH_HOST, // SSH 서버의 퍼블릭 IP
+  port: 22,
+  username: process.env.SSH_USER,
+  privateKey: require('fs').readFileSync(path.join(__dirname, 'aniop2023.pem')) // Updated path
+});
 
 // 모든 기사 조회
 app.get('/api/articles', async (req, res) => {
@@ -75,7 +72,6 @@ app.get('/api/articles', async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
 // 특정 날짜의 기사 조회
 app.post('/api/articles/by-date', async (req, res) => {
     const { date } = req.body;
@@ -104,8 +100,6 @@ app.post('/api/articles/by-id', async (req, res) => {
     }
 });
 
-=======
->>>>>>> 1ae337f027553786a20b7f8f126b18a3bed88231
 // 서버 시작
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
