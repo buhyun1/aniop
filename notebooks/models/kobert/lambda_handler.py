009ba7a4e7d99b94ee53e53a9411ced6a0abfe0e
AnYeHyeon <ahnyh4756@gmail.com>
2023-11-22 16:01:09 +0900

    chore: 라이브러리 업데이트

--- notebooks/models/kobert/lambda_handler.py
+++ notebooks/models/kobert/lambda_handler.py
@@ -1,13 +1,14 @@
 import torch
-import transformers
 from transformers import BertConfig, BertTokenizer, BertForSequenceClassification
 import json
 from mymodel import MyModel1
+import boto3
 
 def load_model(model_path, number_of_labels, tokenizer_path='bert-base-uncased'):
     config = BertConfig.from_pretrained(tokenizer_path, num_labels=number_of_labels)
     model = MyModel1(config) # 설정 파일 로드
-    model.load_state_dict(torch.load(model_path))
+
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
     model.eval()  # 평가 모드로 설정
     return model, tokenizer
@@ -19,32 +20,49 @@ def predict(model, tokenizer, input_text):
     probabilities = torch.nn.functional.softmax(outputs.logits, dim=1)
     return probabilities.numpy()
 
-def lambda_handler(event):
-    number_of_labels = 4
-    model_path = './model.pth'
-    model, tokenizer = load_model(model_path, number_of_labels)
-    
-    # 입력 데이터 처리
-    with open(event, 'r') as file:
-        data = json.load(file)
+def lambda_handler(event, context):
+    s3 = boto3.client('s3', 
+                      region_name='ap-northeast-2', 
+                      aws_access_key_id='AKIAZAPACE62LXQRT2WS', 
+                      aws_secret_access_key='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')
+
+    # S3 버킷과 파일 이름
+    bucket = event['bucket']
+    input_file_key = event['input_file']
+    output_file = event['output_file']
+
+    # S3에서 입력 파일 읽기
+    input_obj = s3.get_object(Bucket=bucket, Key=input_file_key)
+    input_data = json.loads(input_obj['Body'].read().decode('utf-8'))
+
+    model, tokenizer = load_model('./model.pth', 4)
     
     # 출력 데이터 준비
-    outdata = data.copy()
+    outdata = input_data.copy()
     
     for article in outdata['news']:
-        input_text = article['title']
+        input_text = article['Title']  # 'Title' 키 사용
         prediction = predict(model, tokenizer, input_text)
-        # 카테고리 예측 결과 추가 (예시: 가장 높은 확률을 가진 카테고리 인덱스)
         predicted_category = prediction.argmax(axis=1).item()
-        article['category'] = predicted_category
+        article['Category'] = predicted_category
 
-    return outdata
+    # 결과를 JSON 문자열로 변환
+    output_json = json.dumps(outdata, ensure_ascii=False)
 
-# 파일 경로
-predict_file_path = './predicted_data.json'
+    # S3에 결과 파일 쓰기
+    s3.put_object(Body=output_json, Bucket=bucket, Key=output_file)
 
-# 함수 실행
-result = lambda_handler(predict_file_path)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File uploaded successfully')
+    }
 
-# 결과 출력
-print(result)
+
+# 로컬 테스트
+if __name__ == "__main__":
+    test_event = {'bucket': 'aniop2023', 
+                  'input_file': 'manual_predicted_news_articles.json', 
+                  'output_file': 'manual_predicted_news_articles.json'}
+    test_context = None
+    result = lambda_handler(test_event, test_context)
+    print(result)
